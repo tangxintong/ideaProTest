8cd9749ea533841c95c2bcc965ee2010243e48e4
tangxintong <972782997@qq.com>
2018-09-14 20:37:39 +0800

    this a test 2

---
+++ src/main/java/Ideatest.java
@@ -0,0 +1,20 @@
+public class Ideatest {
+    private String Name;
+    private  Number Tel;
+
+    public String getName() {
+        return Name;
+    }
+
+    public Number getTel() {
+        return Tel;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setTel(Number tel) {
+        Tel = tel;
+    }
+}
